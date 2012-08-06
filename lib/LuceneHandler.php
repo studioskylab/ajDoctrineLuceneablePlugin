8669fdfb84d9fab269c75dd85edfca4c558b5301
Jaik Dean <jaik@studioskylab.com>
2012-08-06 11:41:54 +0100

    Added StandardAnalyzer to allow fuzzy plural searching

--- lib/LuceneHandler.php
+++ lib/LuceneHandler.php
@@ -17,6 +17,7 @@ class LuceneHandler {
       {
           return self::$_index;
       }
+
       self::autoload();
       if (file_exists($index = self::getLuceneIndexFile()))
       {
@@ -36,12 +37,14 @@ class LuceneHandler {
         {
             self::registerZend();
         } else {
-			if (sfContext::hasInstance()) {
-				$dispatcher = sfContext::getInstance()->getEventDispatcher();
-				$dispatcher->notify(new sfEvent(new stdClass(),'luceneable.autoload', array()));
-			}
+          if (sfContext::hasInstance()) {
+            $dispatcher = sfContext::getInstance()->getEventDispatcher();
+            $dispatcher->notify(new sfEvent(new stdClass(),'luceneable.autoload', array()));
+          }
         }
 
+        // set the Lucene analyzer to allow fuzzy plural searching
+        Zend_Search_Lucene_Analysis_Analyzer::setDefault(new StandardAnalyzer_Analyzer_Standard_English());
     }
     
       static protected $zendLoaded = false;
